47a172d4437c13d957c1ded7fb4a597cc2a6fcf6
zk <271226192@qq.com>
2020-04-18 07:41:00 +0800

    init project.

---
+++ app.js
@@ -0,0 +1,11 @@
+// app.js
+
+const http = require("http");
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<h1>Node.js</h1>');
+    res.end("<p>Hello World</p>");
+}).listen(8848);
+
+console.log('HTTP server is listening at port 8840');
